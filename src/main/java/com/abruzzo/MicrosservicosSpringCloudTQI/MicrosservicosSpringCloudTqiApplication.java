89f22d6c640e1e53645a6b8c94149c4830b0466e
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2021-12-07 23:02:35 -0300

    Kickoff

---
+++ src/main/java/com/abruzzo/MicrosservicosSpringCloudTQI/MicrosservicosSpringCloudTqiApplication.java
@@ -0,0 +1,13 @@
+package com.abruzzo.MicrosservicosSpringCloudTQI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicosSpringCloudTqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicosSpringCloudTqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abruzzo/MicrosservicosSpringCloudTQI/MicrosservicosSpringCloudTqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abruzzo.MicrosservicosSpringCloudTQI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicosSpringCloudTqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
